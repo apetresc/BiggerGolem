ab4ccff48dd54f8d1282c259c47d008ec222f0a0
AdrianP <apetresc@gmail.com>
2010-08-17 02:58:25 -0400

    BiggerGolem: Set default style to "default"

--- src/shogi_piece_set.js
+++ src/shogi_piece_set.js
@@ -4,20 +4,20 @@ var wait_for_response = true;
 
 function set_style(style) {
     if (game_name.indexOf("Shogi") >= 0) {
-        if (style != "default") {
-            var divs = $("div");
-            for (var i = 0; i < divs.length; i++) {
-                var piece_pos = divs[i].style.backgroundImage.search(/shogi\d\d\.png/);
-                if (piece_pos >= 0) {
-                    if (style == "blank") {
-                        divs[i].style.backgroundImage = "shogi00.png";
-                        divs[i].style.visibility = "hidden";
-                    } else {
-                        var piece = divs[i].style.backgroundImage.substr(piece_pos, 7);
+        var divs = $("div");
+        for (var i = 0; i < divs.length; i++) {
+            var piece_pos = divs[i].style.backgroundImage.search(/shogi\d\d\.png/);
+            if (piece_pos >= 0) {
+                if (style == "blank") {
+                    divs[i].style.backgroundImage = "shogi00.png";
+                    divs[i].style.visibility = "hidden";
+                } else {
+                    var piece = divs[i].style.backgroundImage.substr(piece_pos, 7);
+                    if (style != "default") {
                         divs[i].style.backgroundImage = "url(" + chrome.extension.getURL("img/shogi/kanji/" + style + "/" + piece + ".png") + ")";
                         divs[i].style.height = "48px";
-                        divs[i].style.visibility = "visible";
                     }
+                    divs[i].style.visibility = "visible";
                 }
             }
         }
@@ -27,6 +27,7 @@ function set_style(style) {
 set_style("blank");
 chrome.extension.sendRequest({localstorage: "style"},
                              function(response) {
+                                if (response.style == undefined) response.style = "default";
                                 set_style(response.style);
                                 wait_for_response = false;
                              });
