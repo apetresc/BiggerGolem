13f417dde0a89120c37ba87b95dac4760e3b74bf
U-Trevor-PC\Trevor <Trevor@Trevor-PC.(none)>
2010-09-26 19:33:20 -0700

    added Reversi improvements

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+var game_name = $("body>table:nth-child(3)>tbody>tr>td>font>b").text();
+
+if (game_name.indexOf("Reversi") >= 0) {
+	chrome.extension.sendRequest({localstorage: "reversiStyle"},
+		function(response) {
+			set_reversi_style(response.green,response.go);
+		});
+}
+
+if (game_name.indexOf("Shogi") >= 0) {
+	set_style("blank");
+	chrome.extension.sendRequest({localstorage: "style"},
+		function(response) {
+			set_style(response.style);
+		});
+}
