57a02ad0f6e60c018eaa23095726d04a5cecdcd4
Mathijs Claassen <mathijs.claassen@gmail.com>
2017-09-22 16:37:33 +0200

    better analyse experience than Jago

---
+++ src/go_analyse.js
@@ -0,0 +1,63 @@
+var analyseMode = false
+var game, moves;
+
+function go_analyse() {
+    $('body').prepend('<style> .lg-board {width: ' + $('table').width() +'px;} </style>')
+    let btn = $('<button id="analyse" class="btn blue">Analyse</button>')
+    btn.click(function(){
+        toggleAnalyseMode()
+    })
+    $('i.fa-fast-backward').closest('div').append(btn)
+
+    $('i.fa-fast-backward, i.fa-backward').click(function(e) {
+        if (analyseMode) {
+            game.undo()
+            e.preventDefault()
+        }
+    })
+
+    $('i.fa-fast-forward, i.fa-forward').click(function(e) {
+        if (analyseMode) {
+            e.preventDefault()
+        }
+    })
+}
+
+function characterToCoordinate(c) {
+    return  (c.charCodeAt(0) - 97);
+}
+
+function playSGF() {
+    for (i=0;i<moves.length; i++) {
+        if (moves[i].substr(2,2) == 'tt') {
+            game.pass()
+        } else {
+            game.playAt(characterToCoordinate(moves[i][3]), characterToCoordinate(moves[i][2]))
+        }
+    }
+}
+
+
+function toggleAnalyseMode() {
+    analyseMode = !analyseMode
+    $('button#analyse').toggleClass('blue').toggleClass('red')
+    if (analyseMode) {
+        $('table').hide()
+        $('table').parent().append('<div class="lg-board tenuki-board" data-include-coordinates="true"></div>')
+        var boardElement = document.querySelector(".tenuki-board");
+        game = new tenuki.Game({ element: boardElement });
+        if (moves) {
+            playSGF()
+        } else {
+            var sgffile = $('div.actions a').attr('href')
+            $.get( sgffile, function( data ) {
+                moves = data.match(/[B|W]\[(..)\]/g);
+                playSGF()
+              });
+        }
+    } else {
+        $('div.tenuki-board').remove()
+        $('table').show()
+    }
+}
+
