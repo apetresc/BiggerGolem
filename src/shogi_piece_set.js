390002f6bff50e82cf3c9321cbc324da7caa40c2
Adrian Petrescu <apetresc@gmail.com>
2018-01-17 17:01:33 -0500

    Redo shogi styling

--- src/shogi_piece_set.js
+++ src/shogi_piece_set.js
@@ -3,26 +3,23 @@ function set_style(style) {
     return;
   }
 
-  var extension_prefix = chrome.extension.getURL();
-  var divs = $("div");
-  for (var i = 0; i < divs.length; i++) {
-    var piece_pos = divs[i].style.backgroundImage.search(/shogi\d\d/);
-    if (piece_pos >= 0) {
-      var piece = divs[i].style.backgroundImage.substr(piece_pos, 7);
-      divs[i].style.backgroundImage = "url(" + extension_prefix + "img/shogi/kanji/" + style + "/" + piece + ".png" + ")";
-      divs[i].style.height = "48px";
-    }
-  }
+  var extension_prefix = chrome.extension.getURL('');
+  
+  $("div.col-md-12 div").each(function(i) {
+      var piece_pos = this.style.backgroundImage.search(/shogi\d\d/);
+      if (piece_pos >= 0) {
+          var piece = this.style.backgroundImage.substr(piece_pos, 7);
+          this.style.backgroundImage = "url(" + extension_prefix + "img/shogi/kanji/" + style + "/" + piece + ".png" + ")";
+          this.style.height = "48px";
+      }
+  });
 
   // Might also be a promotion box.
-  if ($("body>table:nth-child(3) div").length == 43) {
-    promotion_pieces = $("body>table:nth-child(3) div:eq(41)>img");
-    for (var i = 0; i < promotion_pieces.length; i++) {
-      var piece = promotion_pieces[i].className;
-      if (piece == undefined || piece == "") {
-        piece = promotion_pieces[i].src.substr(promotion_pieces[i].src.search(/shogi\d\d/), 7);
-      }
-      promotion_pieces[i].src = "url(" + extension_prefix + "img/shogi/kanji/" + style + "/" + piece + ".png" + ")";
-    }
-  }
+  $('img').each(function(i) {
+    var piece_pos = this.src.search(/shogi\d\d/);
+    if (piece_pos >= 0) {
+        var piece = this.src.substr(piece_pos, 7);
+        this.src = extension_prefix + "img/shogi/kanji/" + style + "/" + piece + ".png";
+    }  
+  });
 }
