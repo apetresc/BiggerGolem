a60111c8e92091f2cd96f6b14e8e473ad321dc6d
Adrian Petrescu <apetresc@gmail.com>
2018-01-17 16:26:24 -0500

    Major performance improvement for chess styling

--- src/chess_style.js
+++ src/chess_style.js
@@ -1,18 +1,7 @@
-var pieceRegex = /^.*\/([wb][prnbqk])\.gif$/;
-
-function chess_blank() {
-  $('body table').css('visibility', 'hidden');
-  $('body table img').each(function(i, element) {
-    var piece = pieceRegex.exec(element.src);
-    if (piece) {
-      $(element).attr('data-piece', piece[1][0] + piece[1][1].toUpperCase());
-      element.src = '';
-    }
-  });
-  $('body table img').attr('img', '');
-}
-
 function chess_style(pieces, size) {
+  var pieceRegex = /^.*\/([wb][prnbqk])\.gif$/;
+  var extension_prefix = chrome.extension.getURL('');
+
   if (size != '32') {
     $('body table td').css('width', size + 'px')
     $('body table td').css('height', size + 'px')
@@ -20,16 +9,13 @@ function chess_style(pieces, size) {
     $('body table img').css('height', size + 'px')
   }
 
-  $('body table img').each(function(i, element) {
-    var piece = $(element).attr('data-piece');
-    if (typeof piece !== typeof undefined && piece !== false) {
-      if (pieces == 'default') {
-        element.src = '/ng/images/source/chess/' + piece.toLowerCase() + '.gif';
-      } else {
-        element.src = chrome.extension.getURL('img/chess/' + pieces + '/' + piece + '.svg');
+  if (pieces != 'default') {
+    $('body table img').each(function(i, element) {
+      var piece = pieceRegex.exec(element.src);
+      if (piece !== null) {
+        piece = piece[1][0] + piece[1][1].toUpperCase();
+        element.src = extension_prefix + 'img/chess/' + pieces + '/' + piece + '.svg';
       }
-    }
-  });
-
-  $('body table').css('visibility', 'visible');
+    });
+  }
 }
--- src/shogi_piece_set.js
+++ src/shogi_piece_set.js
@@ -1,40 +1,28 @@
-
 function set_style(style) {
-    var divs = $("div");
-    for (var i = 0; i < divs.length; i++) {
-        var piece_pos = divs[i].style.backgroundImage.search(/shogi\d\d/);
-        if (piece_pos >= 0) {
-            if (style == "blank") {
-                divs[i].style.backgroundImage = "/*" + divs[i].style.backgroundImage.substr(piece_pos, 7) + "*/";
-                divs[i].style.visibility = "hidden";
-            } else {
-                var piece = divs[i].style.backgroundImage.substr(piece_pos, 7);
-                if (style != "default") {
-                    divs[i].style.backgroundImage = "url(" + chrome.extension.getURL("img/shogi/kanji/" + style + "/" + piece + ".png") + ")";
-                    divs[i].style.height = "48px";
-                }
-                divs[i].style.visibility = "visible";
-            }
-        }
-    }
+  if (style == "default") {
+    return;
+  }
 
-    // Might also be a promotion box.
-    if ($("body>table:nth-child(3) div").length == 43) {
-        promotion_pieces = $("body>table:nth-child(3) div:eq(41)>img");
-        for (var i = 0; i < promotion_pieces.length; i++) {
-            if (style == "blank") {
-                var piece_pos = promotion_pieces[i].src.search(/shogi\d\d/);
-                promotion_pieces[i].className = promotion_pieces[i].src.substr(piece_pos, 7);
-                //promotion_pieces[i].removeAttribute("src");
-                promotion_pieces[i].style.visibility = "hidden";
-            } else {
-                var piece = promotion_pieces[i].className;
-                if (piece == undefined || piece == "") { piece = promotion_pieces[i].src.substr(promotion_pieces[i].src.search(/shogi\d\d/), 7); }
-                if (style != "default") {
-                    promotion_pieces[i].src = "url(" + chrome.extension.getURL("img/shogi/kanji/" + style + "/" + piece + ".png") + ")";
-                }
-                promotion_pieces[i].style.visibility = "visible";
-            }
-        }
+  var extension_prefix = chrome.extension.getURL();
+  var divs = $("div");
+  for (var i = 0; i < divs.length; i++) {
+    var piece_pos = divs[i].style.backgroundImage.search(/shogi\d\d/);
+    if (piece_pos >= 0) {
+      var piece = divs[i].style.backgroundImage.substr(piece_pos, 7);
+      divs[i].style.backgroundImage = "url(" + extension_prefix + "img/shogi/kanji/" + style + "/" + piece + ".png" + ")";
+      divs[i].style.height = "48px";
     }
+  }
+
+  // Might also be a promotion box.
+  if ($("body>table:nth-child(3) div").length == 43) {
+    promotion_pieces = $("body>table:nth-child(3) div:eq(41)>img");
+    for (var i = 0; i < promotion_pieces.length; i++) {
+      var piece = promotion_pieces[i].className;
+      if (piece == undefined || piece == "") {
+        piece = promotion_pieces[i].src.substr(promotion_pieces[i].src.search(/shogi\d\d/), 7);
+      }
+      promotion_pieces[i].src = "url(" + extension_prefix + "img/shogi/kanji/" + style + "/" + piece + ".png" + ")";
+    }
+  }
 }
